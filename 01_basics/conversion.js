c96fef4fe4f5e3ea9ee4e91511f7100f7e47d134
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-15 05:24:05 +0000

    conversion

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,6 +1,15 @@
-let score="33ab"
+let score=false
+
 console.log(typeof score);
 console.log(typeof(score));
 console.log(score);
 let valueInNumber=Number(score);
 console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33"=>33
+// "33abc"=> Nan
+// "true"=> 1
+// "false"=>0
+let isLoggedIn=true;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(isLoggedIn);
