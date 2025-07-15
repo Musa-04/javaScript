1f187aec0dc2835d0331f630366143f41dbb3b37
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-15 15:07:54 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+//***************operations**************
+let value=3
+let negValue=-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-5);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="hello"
+// let str2=" Musa"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");  //32
+// console.log(3+4*5%3-10);
+
+console.log(true);
+console.log(+"");
+let num1, num2,num3
+num1=num2=num3=2+2
+console.log(num1);
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
