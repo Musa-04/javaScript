b77c7857fbc2fe33e33ae7ec5d416873348bc049
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-16 11:48:08 +0000

    strings

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,3 +1,18 @@
 const name="musa"
 const repoCount=50
-console.log(name+repoCount+" value");
+console.log(name+ repoCount+" value");
+console.log(`hello my name is ${name} and my repo count us ${repoCount}`); //string interpolation 
+
+const gameName=new String('musa-ms');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase()); 
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('m'));
+const newString=gameName.substring(0,3)
+console.log(newString);
+
+
+const anotherString=gameName.slice(-6,3)
