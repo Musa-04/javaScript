d4092e94427493f15fa133bc0ed936a367a92076
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-16 19:23:21 +0000

    numsandmaths

---
+++ 01_basics/numsandmaths.js
@@ -0,0 +1,30 @@
+const score=400
+console.log(score);
+
+
+const balance=new Number(100)
+console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // to fixed is used in ecommerce websites for fixing the numbers OUTPUT:-?100.00
+
+const newNumber=123.58922
+console.log(newNumber.toPrecision(10));
+ const hundreds=100000
+//  console.log(hundreds.toLocaleString('en-IN'));
+ 
+ 
+ //+++++++++Maths++++++++++++++++
+
+ console.log(Math);
+ console.log(Math.abs(-5));
+ console.log(Math.round(4.46));
+ console.log(Math.ceil(4.2));
+ console.log(Math.floor(4.56));
+console.log(Math.min(4,3,6,5,8,4,5,5));
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const Min=10
+const Max=20
+console.log(Math.floor(Math.random()  *  ( Max- Min + 1 ))+Min)
