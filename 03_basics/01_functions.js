fc48d5e9d9b46121b27eb336f69918b8406af690
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-18 17:38:27 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,25 @@
+// console.log("H");
+// console.log("e");
+function sayMyName(){
+    console.log("M");
+    console.log("U");
+    console.log("S"); //
+    console.log("A");
+    
+}
+// sayMyName()
+// function addTwoNumbers(num1,num2){
+//    console.log(num1+num2);
+
+// }
+
+function addTwoNumbers(num1,num2){
+    let result=num1+num2
+    return result
+    
+
+}
+const result= addTwoNumbers(5,8)
+console.log("Result:",result);
+
+
