731ac72487f6394727c16da87bd9f52a20115931
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-20 13:35:35 +0000

    scopes in javaScript

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,16 @@
+let a=100
+const b=20
+// let c=300
+
+ if (true){
+    let a=10
+    const b=20
+    console.log("Inner Log:",a); //
+    
+    
+ }
+
+ 
+console.log(a);
+// console.log(b);
+// console.log(c);
