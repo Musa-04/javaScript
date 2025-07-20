80564310a5d908648de929ff701bdfb9e6b13759
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-20 18:08:17 +0000

    IIFE in javaScript .

---
+++ 03_basics/iife.js
@@ -0,0 +1,16 @@
+(function chai(){
+    console.log(`DB is connected`);
+    
+})();
+// This is an IIFE (Immediately Invoked Function Expression) that logs a message to the console when the script is executed.
+
+(function chai(){
+    console.log(`DB is connected successfully`);
+    
+})(); // This is an IIFE (Immediately Invoked Function Expression) that logs a message to the console when the script is executed.
+// This is an IIFE (Immediately Invoked Function Expression) that logs a message to the
+
+((name)=>{
+    console.log(`DB is connected successfully Two ${name}`);
+    
+})('musa'); // This is an IIFE (Immediately Invoked Function Expression) that logs a message to the console when the script is executed.
