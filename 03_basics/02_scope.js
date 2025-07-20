82b65f4869ddfedb16ce92a6c225942622411761
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-20 14:44:43 +0000

    scopes in javaScript

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,3 +1,4 @@
+
 let a=100
 const b=20
 // let c=300
@@ -5,12 +6,53 @@ const b=20
  if (true){
     let a=10
     const b=20
-    console.log("Inner Log:",a); //
+    // console.log("Inner Log:",a); //
     
     
  }
 
- 
-console.log(a);
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// function one(){
+//     const username="John"
+//     function two(){
+//         const website="example.com"
+//         console.log(username);
+        
+//     }
+
+//     // console.log(website);
+//     // two();
+// }
+// one();
+
+    // two();
+
+    if(true){
+        const username="Jane"
+        if(username==="Jane"){
+            const website="  example.com"
+            // console.log(username + website);
+            
+        }
+        // console.log(website);
+
+        
+    }
+    // console.log(username);
+    
+
+addone(10)
+console.log();
+
+function addone(num){
+    return  num+1
+}
+
+const addTwo=function(num){
+    return num+2
+}
+addTwo(18)
