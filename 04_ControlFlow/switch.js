a8b0c10bafb889af1377c816d928937f059a733a
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-21 17:32:39 +0000

    switch in js

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,29 @@
+// switch(key){
+
+// }
+const month=3
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("Fab");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("default");
+        
+        break;
+}
+
+
