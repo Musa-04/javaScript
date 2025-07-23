b144782ccad769c3f25c73d29000971d3ee0065f
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-23 18:18:09 +0000

    for of in js

--- 05_loops/three.js
+++ 05_loops/three.js
@@ -1,6 +1,33 @@
 //for of
 
 const arr=[1,2,3,4,5]
-for (const element of object) {
+for (const element of arr) {
+    // console.log(element);
+}
+
+const greetings="hello world!"
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
+    
+    
+}
+//Maps 
+const map= new Map()
+map.set('IN ',"india")
+map.set('USA',"United States of america")
+map.set('Fr',"France")
+console.log(map);
+
+for (const [key,value] of map) {
+    
+    // console.log(key,':-',value);
+    
+}
+
+const myObject={
+    'game1':'NFS',
+    'game2':'spi'
+}
+for (const [key,value] of myObject) {
     
 }
