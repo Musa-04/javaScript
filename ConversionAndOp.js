a9f4113ede9f153d571ae197a551c229a946f33d
Mustafeez Shaikh <mustafeezshaikh88@gmail.com>
2025-07-31 23:10:33 +0530

    Add files via upload

---
+++ ConversionAndOp.js
@@ -0,0 +1,9 @@
+let num=33
+let score=null
+
+console.log(typeof score); 
+console.log(typeof num);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
